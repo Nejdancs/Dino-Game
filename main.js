6fa0b324ea0d3adca5801935369bb78b510c5271
Sergey Korobka <nejdancs@gmail.com>
2022-06-02 00:09:55 +0300

    commit

---
+++ main.js
@@ -0,0 +1,167 @@
+const refs = {
+  dino: document.querySelector(".dino"),
+  wrapCactus: document.querySelector(".wrap-cactus"),
+  ground: document.querySelector(".ground"),
+  cloud: document.querySelector(".cloud"),
+  gameOver: document.querySelector(".game-over"),
+  topScore: document.querySelector(".top-scor"),
+  currentScore: document.querySelector(".current-scor"),
+};
+
+let jumpTimeoutId;
+let topScore = localStorage.getItem("topScore") ?? 0;
+let currentScore = 0;
+let scoreIntId;
+
+startGame();
+
+function startGame() {
+  refs.topScore.innerHTML = String(topScore).padStart(5, "0");
+  addEventListener("click", jumpHandler);
+  addEventListener("keydown", jumpHandler);
+
+  addEventListener("click", onStart);
+  addEventListener("keydown", onStart);
+}
+
+function onStart(e) {
+  if (e.code !== "Space" && e.type !== "click") {
+    return;
+  }
+  currentScore = 0;
+
+  clearClassList();
+  counterCurrentScore();
+  cactusRun();
+
+  refs.wrapCactus.innerHTML = createCactus();
+
+  refs.dino.classList.add("dino-run");
+
+  removeEventListener("click", onStart);
+  removeEventListener("keydown", onStart);
+  checkResult();
+}
+
+function counterCurrentScore() {
+  scoreIntId = setInterval(() => {
+    currentScore += 1;
+
+    refs.currentScore.innerHTML = String(currentScore).padStart(5, "0");
+  }, 100);
+}
+
+function clearClassList() {
+  refs.wrapCactus.classList.remove("cactus-run");
+  refs.ground.classList.remove("ground-run");
+  refs.wrapCactus.classList.remove("cactus-stop");
+  refs.dino.classList.remove("dino-stop");
+  refs.dino.classList.remove("dino-crash");
+  refs.ground.classList.remove("ground-stop");
+  refs.cloud.classList.remove("cloud-stop");
+  refs.dino.classList.remove("dino-stay");
+  refs.gameOver.classList.add("hidden");
+}
+
+const cactus = document.querySelector(".cactus");
+
+function createCactus() {
+  let cactusMarkup = "";
+  const { randomAmount, ramdomSize } = generateSizeAndAmount();
+
+  for (let i = 1; i <= randomAmount; i += 1) {
+    cactusMarkup += `<div class="cactus" style="width:${
+      ramdomSize / 1.66
+    }px ; height: ${ramdomSize}px;"></div>`;
+  }
+  return cactusMarkup;
+}
+
+function cactusRun() {
+  refs.wrapCactus.addEventListener("animationend", animationEndHandler);
+
+  setTimeout(() => {
+    refs.wrapCactus.classList.add("cactus-run");
+    refs.ground.classList.add("ground-run");
+    refs.cloud.classList.add("cloud-run");
+  }, 0);
+}
+
+function generateSizeAndAmount() {
+  let max = 60;
+  let min = 30;
+  const randomAmount = Math.round(Math.random() * (4 - 1) + 1);
+
+  if (randomAmount === 4) {
+    max = 40;
+  } else if (randomAmount === 3) {
+    max = 50;
+  } else if (randomAmount === 2) {
+    max = 60;
+  } else if (randomAmount === 1) {
+    max = 80;
+  }
+
+  const ramdomSize = Math.round(Math.random() * (max - min) + min);
+
+  return { randomAmount, ramdomSize };
+}
+
+function jumpHandler(e) {
+  if (e.code !== "Space" && e.type !== "click") {
+    return;
+  }
+  refs.dino.classList.add("dino-jump");
+  jumpTimeoutId = setTimeout(() => {
+    refs.dino.classList.remove("dino-jump");
+  }, 500);
+}
+
+function checkResult() {
+  const intId = setInterval(() => {
+    const posDino = refs.dino.getBoundingClientRect();
+    const posCactus = refs.wrapCactus.getBoundingClientRect();
+
+    if (
+      posDino.bottom - 10 >= posCactus.top + 10 &&
+      posCactus.left <= posDino.right - 10 &&
+      posCactus.right >= posDino.left + 10
+    ) {
+      clearTimeout(jumpTimeoutId);
+      clearInterval(intId);
+
+      removeEventListener("click", jumpHandler);
+      removeEventListener("keydown", jumpHandler);
+
+      refs.wrapCactus.removeEventListener("animationend", animationEndHandler);
+      topScore = String(
+        topScore < currentScore ? currentScore : topScore
+      ).padStart(5, "0");
+
+      localStorage.setItem("topScore", topScore);
+
+      setClassList();
+      startGame();
+      clearInterval(scoreIntId);
+      refs.gameOver.classList.remove("hidden");
+    }
+  }, 0);
+}
+
+function setClassList() {
+  refs.dino.classList.remove("dino-run");
+  refs.dino.classList.add("dino-crash");
+  refs.dino.classList.add("dino-stop");
+  refs.wrapCactus.classList.add("cactus-stop");
+  refs.ground.classList.add("ground-stop");
+  refs.cloud.classList.add("cloud-stop");
+}
+
+function animationEndHandler() {
+  refs.wrapCactus.classList.remove("cactus-run");
+  refs.wrapCactus.removeEventListener("animationend", animationEndHandler);
+  refs.wrapCactus.innerHTML = createCactus();
+  cactusRun();
+}
+
+function createGameOverMarkup() {}
